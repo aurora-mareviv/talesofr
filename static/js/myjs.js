0bb3c0e21f2ae71cec3917e72dd196727bddc5ed
aurora-mareviv <mariauror@gmail.com>
2018-03-30 03:20:15 +0200

    Updating blog

--- static/js/myjs.js
+++ static/js/myjs.js
@@ -1,33 +1,33 @@
 // silly js demo
 
-function alertInfoIcon() {
+function alertInfoIcon(myID) {
   var result = "<span class='fa fa-info-circle'></span>&nbsp;alert-info";
   document.getElementById("toInfo").innerHTML = result; 
-  document.getElementById("myButton").style.backgroundColor = "#d9edf7";
-  document.getElementById("myButton").style.color = "#3a87ad";
-  document.getElementById("myButton").style.borderColor = "#bce8f1";
+  document.getElementById(myID).style.backgroundColor = "#d9edf7";
+  document.getElementById(myID).style.color = "#3a87ad";
+  document.getElementById(myID).style.borderColor = "#bce8f1";
 }
 
-function alertInfo() {
-  document.getElementById("myButton").style.backgroundColor = "#d9edf7";
-  document.getElementById("myButton").style.color = "#3a87ad";
-  document.getElementById("myButton").style.borderColor = "#bce8f1";
+function alertInfo(myID) {
+  document.getElementById(myID).style.backgroundColor = "#d9edf7";
+  document.getElementById(myID).style.color = "#3a87ad";
+  document.getElementById(myID).style.borderColor = "#bce8f1";
 }
 
-function alertWarning() {
-  document.getElementById("myButton").style.backgroundColor = "#fcf8e3";
-  document.getElementById("myButton").style.color = "#8a6d3b";
-  document.getElementById("myButton").style.borderColor = "#faebcc";
+function alertWarning(myID) {
+  document.getElementById(myID).style.backgroundColor = "#fcf8e3";
+  document.getElementById(myID).style.color = "#8a6d3b";
+  document.getElementById(myID).style.borderColor = "#faebcc";
 }
 
-function alertSuccess() {
-  document.getElementById("myButton").style.backgroundColor = "#dff0d8";
-  document.getElementById("myButton").style.color = "#3c763d";
-  document.getElementById("myButton").style.borderColor = "#d6e9c6";
+function alertSuccess(myID) {
+  document.getElementById(myID).style.backgroundColor = "#dff0d8";
+  document.getElementById(myID).style.color = "#3c763d";
+  document.getElementById(myID).style.borderColor = "#d6e9c6";
 }
 
-function alertDanger() {
-  document.getElementById("myButton").style.backgroundColor = "#f2dede";
-  document.getElementById("myButton").style.color = "#b94a48";
-  document.getElementById("myButton").style.borderColor = "#eed3d7";
+function alertDanger(myID) {
+  document.getElementById(myID).style.backgroundColor = "#f2dede";
+  document.getElementById(myID).style.color = "#b94a48";
+  document.getElementById(myID).style.borderColor = "#eed3d7";
 }
